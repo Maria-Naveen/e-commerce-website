e5384864ebdf585aeac0fcc940ff2c842d14c411
Maria Naveen <marianaveen15@outlook.com>
2024-09-22 21:47:53 +0530

    added toast notifications in the login page

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import { ToastContainer, toast, Bounce } from "react-toastify"; // Import Bounce
+
 import LoginImage from "../assets/login.svg";
 
 const Login = () => {
@@ -7,7 +9,7 @@ const Login = () => {
   const [password, setPassword] = useState("");
   const navigate = useNavigate();
 
-  //The following checks whether the user is already login.If true,it returns them to the home page.
+  // The following checks whether the user is already logged in. If true, it redirects them to the home page.
   useEffect(() => {
     const alreadyLoggedIn = sessionStorage.getItem("isLoggedIn");
     if (alreadyLoggedIn) {
@@ -18,24 +20,44 @@ const Login = () => {
   const handleLogin = (e) => {
     e.preventDefault();
 
-    //login logic
+    // Login logic
     if (username === "Naveen" && password === "Naveen") {
+      toast.success("Welcome!");
       sessionStorage.setItem("isLoggedIn", "true");
-      //setTimeOut to clear the session after 15 mins
+
+      // Set timeout to clear the session after 15 mins
       setTimeout(() => {
         sessionStorage.removeItem("isLoggedIn");
       }, 15 * 60 * 1000);
-      navigate("/"); //Redirect to home page after successful login
+      setTimeout(() => {
+        navigate("/");
+      }, 1000); // Redirect to home page after successful login
+    } else {
+      toast.error("Invalid login credentials");
     }
   };
+
   return (
     <div className="min-h-screen bg-blue-200 p-4 flex flex-col md:flex-row justify-center items-center">
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+        transition={Bounce} // Corrected transition property
+      />
       <img
         src={LoginImage}
         alt="login"
         className="mt-6 md:mt-0 md:ml-6 w-full max-w-xs"
       />
-      <div className="flex flex-col items-center md:items-start p-6 ">
+      <div className="flex flex-col items-center md:items-start p-6">
         <h1 className="text-xl font-bold mb-4">MyStore Login</h1>
         <form onSubmit={handleLogin} className="flex flex-col w-full max-w-sm">
           <label htmlFor="username" className="mb-1">
